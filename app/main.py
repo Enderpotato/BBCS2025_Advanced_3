a61e2e2e7bd31063b47d5b14cb0310e2502bdcd7
oiu890 <144079179+oiu890@users.noreply.github.com>
2025-06-13 18:05:20 +0800

    Update main.py
    fixed audio (i think)

--- app/main.py
+++ app/main.py
@@ -5,39 +5,40 @@ import time
 app = Flask(__name__)
 cam = cv2.VideoCapture(0)
 
+last_played = 0
+audio_cooldown = 10 
+
+def is_distracted(frame):
+    #hi enoch put the model logic here or smt return true if distracted
+    return True
+
+def play_sound_once():
+    global last_played
+    now = time.time()
+    if now - last_played > audio_cooldown:
+        last_played = now
+        threading.Thread(target=pygame.mixer.music.load, args=("static/focus.mp3",)).start()
+        threading.Thread(target=pygame.mixer.music.play).start()
+
+
+
 def gen_frames():
     while True:
-    #iteratively write the current frame to the output file
         okay, frame = cam.read() #ret is boolean, True if successful
         if not okay:break
         
+        if is_distracted(frame):
+            play_sound_once()
+
         ret, buffer = cv2.imencode('.jpg',frame)
         frame = buffer.tobytes()
         
         yield(b'--frame\r\n'
-              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n  ')
-
-audioLength = 10
-timeLastAudioPlayed = -audioLength
-    
+              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route("/")
 def hi_enoch():
-    global timeLastAudioPlayed
-    isDistracted = True
-    playingAudio = True
-
-    startAudio = False
-    print(audioLength)
-    if time.time() - timeLastAudioPlayed > audioLength:
-        playingAudio = False
-
-    if isDistracted and not playingAudio:
-        startAudio = True
-        playingAudio = True
-        timeLastAudioPlayed = time.time()
-
-    return render_template('index.html', startAudio = isDistracted)
+    return render_template('index.html')
 
 @app.route("/video")
 def video():
