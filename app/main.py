45e4ff3e093c50fe73a9029ec6dfd68f28963818
oiu890 <144079179+oiu890@users.noreply.github.com>
2025-06-13 15:53:20 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,2 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,Response
 import cv2
+
+app = Flask(__name__)
+cam = cv2.VideoCapture(0)
+
+def gen_frames():
+    while True:
+    #iteratively write the current frame to the output file
+        okay, frame = cam.read() #ret is boolean, True if successful
+        if not okay:break
+        
+        ret, buffer = cv2.imencode('.jpg',frame)
+        frame = buffer.tobytes()
+        
+        yield(b'--frame\r\n'
+              b'Content-Type: image')
+
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+
+@app.route("/")
+def hi_enoch():
+    return render_template('index.html')
+
+@app.route("/video")
+def video():
+    return Response(gen_frames)
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
