098c26c98ffe00af420b9b87ac1ee78a53226b79
oiu890 <144079179+oiu890@users.noreply.github.com>
2025-06-13 21:37:10 +0800

    Update main.py
    minor edit

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,Response
+from flask import Flask, render_template,Response,jsonify
 import cv2
 import time
 import threading
@@ -7,21 +7,31 @@ import pygame
 
 app = Flask(__name__)
 cam = cv2.VideoCapture(0)
+pygame.init()
+pygame.mixer.init()
+
 
 last_played = 0
 audio_cooldown = 10 
+is_playing_audio = False
 
 def is_distracted(frame):
     #hi enoch put the model logic here or smt return true if distracted
     return True
 
 def play_sound_once():
-    global last_played
+    global last_played, is_playing_audio
     now = time.time()
     if now - last_played > audio_cooldown:
         last_played = now
-        threading.Thread(target=pygame.mixer.music.load, args=("static/wakeup.mp3",)).start()
-        threading.Thread(target=pygame.mixer.music.play).start()
+        is_playing_audio = True
+        def play():
+            pygame.mixer.music.load("static/wakeup.mp3")
+            pygame.mixer.music.play()
+            time.sleep(2)
+            global is_playing_audio
+            is_playing_audio = False
+        threading.Thread(target=play, daemon=True).start()
 
 
 def gen_frames():
@@ -49,10 +59,12 @@ def video():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
+@app.route("/status")
+def status():
+    return jsonify({"distracted": is_playing_audio})
+
+
 
 
 if __name__ == "__main__":
-    # timeLastAudioPlayed = -audioLength
-    pygame.init()
-    pygame.mixer.init()
     app.run(debug = True)
