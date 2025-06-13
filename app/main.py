7fbf8867ec493665f11bed7f4d1850cc5b2bb4ec
Hengweez <93776535+Hengweez@users.noreply.github.com>
2025-06-13 17:17:24 +0800

    awdawd

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,Response
 import cv2
+import time
 
 app = Flask(__name__)
 cam = cv2.VideoCapture(0)
@@ -16,17 +17,36 @@ def gen_frames():
         yield(b'--frame\r\n'
               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n  ')
 
-
+audioLength = 10
+timeLastAudioPlayed = -audioLength
     
 
 @app.route("/")
 def hi_enoch():
-    return render_template('index.html')
+    global timeLastAudioPlayed
+    isDistracted = True
+    playingAudio = True
+
+    startAudio = False
+    print(audioLength)
+    if time.time() - timeLastAudioPlayed > audioLength:
+        playingAudio = False
+
+    if isDistracted and not playingAudio:
+        startAudio = True
+        playingAudio = True
+        timeLastAudioPlayed = time.time()
+
+    return render_template('index.html', startAudio = isDistracted)
 
 @app.route("/video")
 def video():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
+
+
 if __name__ == "__main__":
+    timeLastAudioPlayed = -audioLength
     app.run(debug = True)
+
